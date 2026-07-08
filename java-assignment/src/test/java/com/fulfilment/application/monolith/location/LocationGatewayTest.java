da6b85873dd6c2e6959798245d3777bcfea35f51
fares bouzayen <ferisbouzayen@gmail.com>
2026-07-08 17:07:04 +0100

    Add unit tests for LocationGateway.resolveByIdentifier

--- java-assignment/src/test/java/com/fulfilment/application/monolith/location/LocationGatewayTest.java
+++ java-assignment/src/test/java/com/fulfilment/application/monolith/location/LocationGatewayTest.java
@@ -1,18 +1,60 @@
 package com.fulfilment.application.monolith.location;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import com.fulfilment.application.monolith.warehouses.domain.models.Location;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class LocationGatewayTest {
 
+  private LocationGateway locationGateway;
+
+  @BeforeEach
+  public void setUp() {
+    locationGateway = new LocationGateway();
+  }
+
   @Test
-  public void testWhenResolveExistingLocationShouldReturn() {
-    // given
-    // LocationGateway locationGateway = new LocationGateway();
+  public void testResolveExistingLocationByIdentifier_shouldReturnLocation() {
+    Location location = locationGateway.resolveByIdentifier("ZWOLLE-001");
 
-    // when
-    // Location location = locationGateway.resolveByIdentifier("ZWOLLE-001");
+    assertNotNull(location);
+    assertEquals("ZWOLLE-001", location.identification);
+    assertEquals(1, location.maxNumberOfWarehouses);
+    assertEquals(40, location.maxCapacity);
+  }
 
-    // then
-    // assertEquals(location.identification, "ZWOLLE-001");
+  @Test
+  public void testResolveExistingLocation_amsterdam001_shouldReturnLocation() {
+    Location location = locationGateway.resolveByIdentifier("AMSTERDAM-001");
+
+    assertNotNull(location);
+    assertEquals("AMSTERDAM-001", location.identification);
+    assertEquals(5, location.maxNumberOfWarehouses);
+    assertEquals(100, location.maxCapacity);
+  }
+
+  @Test
+  public void testResolveNonExistingLocationByIdentifier_shouldReturnNull() {
+    Location location = locationGateway.resolveByIdentifier("NONEXISTENT-001");
+
+    assertNull(location);
+  }
+
+  @Test
+  public void testResolveWithNullIdentifier_shouldReturnNull() {
+    Location location = locationGateway.resolveByIdentifier(null);
+
+    assertNull(location);
+  }
+
+  @Test
+  public void testResolveWithEmptyIdentifier_shouldReturnNull() {
+    Location location = locationGateway.resolveByIdentifier("");
+
+    assertNull(location);
   }
 }
